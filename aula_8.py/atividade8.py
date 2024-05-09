d7124d284be1a5354161c8a8b3ddca1c2fe9af8d
Proencapro7 <168238268+Proencapro7@users.noreply.github.com>
2024-05-08 21:44:41 -0300

    Funções Random e Condicionais

---
+++ aula_8.py/atividade8.py
@@ -0,0 +1,51 @@
+# **Exercício 1:** Escreva um programa que use a função `range()` para gerar os números pares de 2 a 20 e, em seguida, imprima cada número. Utilize os métodos que vimos até então. 
+print(list(range(21)))
+ 
+
+
+
+
+# **Exercício 2:** Escreva um programa que use a função `range()` para gerar os múltiplos de 5 em 5 até 50.
+print(list(range(5,50,2)))
+
+
+
+
+
+# **Exercício 3:** Escreva um programa que use a função `type()` para verificar o tipo de uma variável.
+numero = 7
+type(numero)
+
+
+
+
+
+# **Exercício 4:** Escreva um programa que use a função `print()` para imprimir uma mensagem de saudação personalizada, incluindo o nome do usuário.
+nome = 'Arthur'
+saudacao = 'ola Arthur,seja bem-vindo'
+
+
+
+
+
+# **Exercício 5:** Escreva um programa que use a função `range()` para gerar os números ímpares de 1 a 10 e, em seguida, calcule e imprima a média desses números.
+
+
+
+
+
+# **Exercício 6:** Descubra o máximo e o mínimo da sequência criada no  exercício 5
+
+
+
+
+
+
+# **Exercício 7:** Ordene a lista  -  [10,56,40,5,4,9,7,1,0,56]
+
+
+
+
+
+
+# **Exercícios 8:**  some todos os valores da lista ordenada no exercício 7
---
+++ aula_8.py/aula_8.py
@@ -0,0 +1,5 @@
+import random
+aleatorio = random.randint(1,10)
+print(aleatorio)
+
+
